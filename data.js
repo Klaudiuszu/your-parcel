f917bc9534dcfd22718f22f88fb3d69b0446a3c7
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2022-02-04 00:53:33 +0100

    create data.js

--- data.js
+++ data.js
@@ -1,6 +1,47 @@
-const dataSource = {};
+const dataSource = {
+  clients:
+  // find.
+  // (item) =>
+  [{
+    number: 123456789,
+    code: 1234
+  },{
+    number: 876543210,
+    code: 0000
+  },{
+    number: 876543210,
+    code: 2345
+  },{
+    number: 876543219,
+    code: 0040
+  },{
+    number: 876543218,
+    code:7456
+  },{
+    number: 876543217,
+    code: 8972
+  },{
+    number: 876543216,
+    code: 2354
+  },{
+    number: 876543215,
+    code: 9874
+  },{
+    number: 876543214,
+    code: 1285
+  },{
+    number: 87654321,
+    code: 6985
+  },{
+    number: 876543213,
+    code: 4456
+  },{
+    number: 876545892,
+    code: 2235
+  }]
+}; 
 
-dataSource.clients = {
-  numbers: [12345678, 98765432, 45635789, 14785269],
-  code: [1234, 7894, 4567, 5648],
-};
+// assign value "="
+// "1" "==" 1 true
+// koercja typów
+// "1" "===" 1 false 
--- main.js
+++ main.js
@@ -1,48 +1,41 @@
+const btn1 = document.querySelector(".normal-button");
+const welcomeScreen = document.querySelector("#welcome-button");
+const inputForm = document.querySelector("#input-form");
+const phoneNumberInput = document.querySelector("#phone-number");
+const deliveryCodeInput = document.querySelector("#delivery-code");
+const form = document.querySelector(".check-order");
 
-
-  const btn1 = document.querySelector(".normal-button");
-  const welcomeScreen =  document.querySelector("#welcome-button");
-  const inputForm =  document.querySelector("#input-form");
-  const phoneNumberInput =  document.querySelector("#phone-number");
-  
-  let phoneNumberValue = "";
-
-
-  function checkOrder(){
-    btn1.addEventListener('click', _ => {
-      welcomeScreen.style.display = "none";
-      inputForm.style.display ="block"
-    })
-    
-    phoneNumberInput.addEventListener("input", event => {
-      const value = event.target.value;
-      if(isNaN(value)){
-        alert("Tylko liczby w zakresie od 0 do 9!");
-        phoneNumberInput.value = ""
-      }
-
-      console.log(phoneNumberValue)
-      phoneNumberValue = value;
-    })
-
-    console.log(dataSource.clients.numbers[1])
-
-    if(phoneNumberValue === dataSource.clients.numbers[0]){
-      alert('da')
+// to utils
+const numericInputUpdate = (input, alertMessage) => {
+  input.addEventListener("input", (event) => {
+    const value = event.target.value;
+    if (isNaN(value)) {
+      alert(alertMessage);
+      phoneNumberInput.value = "";
     }
-    
-
-  }
-
-
-
-
-  const dataSource = {};
-
-dataSource.clients = {
-  numbers: [12345678, 98765432, 45635789, 14785269],
-  code: [1234, 7894, 4567, 5648],
+  });
 };
 
+function checkOrder() {
+  btn1.addEventListener("click", (_) => {
+    welcomeScreen.style.display = "none";
+    inputForm.style.display = "block";
+  });
 
-  checkOrder();
+  numericInputUpdate(phoneNumberInput, "phoneAlert");
+  numericInputUpdate(deliveryCodeInput, "deliveryAlert");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const condition = dataSource.clients.find(
+      (client) =>
+        client.number === parseInt(phoneNumberInput.value) &&
+        client.code === parseInt(deliveryCodeInput.value)
+    );
+
+    console.log(condition)
+
+  });
+}
+
+checkOrder();
